0f8c9e7a9f994c038a4e9e7d5462229f496abb41
ELASLAOUI <nabil.elaslaoui@uha.fr>
2018-05-17 13:07:59 +0200

    register with role

--- models/Role.js
+++ models/Role.js
@@ -12,6 +12,10 @@ class  Role {
     get ROLENAME(){
         return this.row.ROLENAME
     }
+    get ROLEID(){
+        return this.row.ROLEID
+    }
+
     static create(roleName,cb){
         connection.query('INSERT INTO role SET ROLENAME=?',[roleName],(err,result)=>{
             if (err) throw err
@@ -21,6 +25,27 @@ class  Role {
 
     static all(cb){
         connection.query('SELECT * FROM role',(err,rows)=>{
+            if (err) throw err
+            cb(rows.map((row) => new Role(row)))
+        })
+    }
+
+    static update(roleName,id , cb){
+        connection.query('UPDATE role SET ROLENAME= ? WHERE ROLEID = ?', [roleName,id], (err, result) => {
+            if(err) throw err
+            cb(result);
+        })
+    }
+
+    static delete(id, cb){
+        connection.query('DELETE FROM role WHERE ROLEID = ?', [id], (err, result) => {
+            if(err) throw err
+            cb(result)
+        })
+    }
+
+    static getOne(id, cb) {
+        connection.query('SELECT * FROM role WHERE ROLEID = ?', [id], (err, rows) => {
             if (err) throw err
             cb(rows.map((row) => new Role(row)))
         })
