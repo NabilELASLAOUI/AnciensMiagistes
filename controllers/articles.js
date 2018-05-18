ec51528d0db57e703f816cb98ed2117f76b05176
Boubacar Sow <boubacar.sow@uha.fr>
2018-05-18 09:56:04 +0200

    MISE A JOUR UPLOAD DE FICHIER

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path')
+const path = require('path');
+const url = require('url');
+
 const uploadDir = path.join(__dirname, '/..', 'public/uploads/');
 var formidable = require('formidable');
+var fs=require('fs');
 
 let Article = require('../models/Article')
 let Category = require('../models/Category')
@@ -23,22 +26,7 @@ router.get('/add',ensureAuthenticated, function (req, res) {
 
 });
 
-router.get('/edit/:articleid',ensureAuthenticated, function (req, res) {
-    Article.getOne(req.params.articleid, function (elem) {
-        //console.log(elem);
-        Category.all(function(cat){
-            res.render('articles/edit', {article: elem,cat:cat});
-            console.log(elem)
-        });
 
-    })
-});
-
-router.get('/delete/:articleid',ensureAuthenticated, function (req, res) {
-    Article.delete(req.params.articleid, function (elem) {
-        res.redirect('/articles');
-    })
-});
 
 router.post('/doAdd', ensureAuthenticated, function (req, res) {
 
@@ -47,21 +35,56 @@ router.post('/doAdd', ensureAuthenticated, function (req, res) {
     form.keepExtensions = true
     form.uploadDir = uploadDir
     form.parse(req, function(err, fields, files) {
+        var nomFichier='';
+        if(files.ARTICLEDOC.size!==0){
+            var cheminFichier=files.ARTICLEDOC.path.split('\\');
+            nomFichier=cheminFichier[cheminFichier.length-1];
+        }else{
+            var cheminFichier=files.ARTICLEDOC.path.split('\\');
+            if(cheminFichier[cheminFichier.length-1]==='fileTodelete'){
+                fs.unlink('public/uploads/'+cheminFichier[cheminFichier.length-1], function (err) {
+                    if (err) {
+                        console.error(err.toString());
+                    } else {
+                        console.warn('le fichier '+cheminFichier[cheminFichier.length-1] + ' a été supprimé');
+                    }
+                });
+            }
+        }
         if(err) res.render('articles/add', { errors: err})
-        Article.create(fields.USERID, fields.CATEGORYID, fields.ARTICLENAME, fields.ARTICLEDATE, fields.ARTICLEDESC, files.ARTICLEDOC.name, function () {
+        Article.create(fields.USERID, fields.CATEGORYID, fields.ARTICLENAME, fields.ARTICLEDATE, fields.ARTICLEDESC, nomFichier, function () {
             req.flash('success', "Article ajouté avec succès !")
             res.redirect('/articles')
         })
 
     })
     form.on('fileBegin', function (name, file) {
-        if(file.name!=='')
-            file.path = path.join(uploadDir, file.name)
+        if(file.name!=='') {
+            var name='fichier';
+            const [fileName, fileExt] = file.name.split('.')
+            file.path = path.join(uploadDir, `${name}_${new Date().getTime()}.${fileExt}`);
 
+        }else{
+            var name='fileTodelete';
+            file.path = path.join(uploadDir, name);
+            console.log( file.path)
+        }
 
     })
 
 });
+
+router.get('/edit/:articleid',ensureAuthenticated, function (req, res) {
+    Article.getOne(req.params.articleid, function (elem) {
+        //console.log(elem);
+        Category.all(function(cat){
+            res.render('articles/edit', {article: elem,cat:cat});
+            //console.log(elem)
+        });
+
+    })
+});
+
 
 router.post('/doEdit', ensureAuthenticated, function (req, res) {
     var form = new formidable.IncomingForm()
@@ -69,9 +92,25 @@ router.post('/doEdit', ensureAuthenticated, function (req, res) {
     form.keepExtensions = true
     form.uploadDir = uploadDir
     form.parse(req, function(err, fields, files) {
-        if(err) res.render('articles/edit', { errors: err})
-        var doc = files.ARTICLEDOC.name!=='' ? files.ARTICLEDOC.name : fields.ARTICLEDOCNAME;
-        console.log(doc);
+
+        if(err) res.render('articles/edit', { errors: err});
+        if( files.ARTICLEDOC.name!=='') {
+            var cheminFichier = files.ARTICLEDOC.path.split('\\');
+            var nomFichier = cheminFichier[cheminFichier.length - 1];
+        }else{
+            var cheminFichier=files.ARTICLEDOC.path.split('\\');
+            if(cheminFichier[cheminFichier.length-1]==='fileTodelete'){
+                fs.unlink('public/uploads/'+cheminFichier[cheminFichier.length-1], function (err) {
+                    if (err) {
+                        console.error(err.toString());
+                    } else {
+                        console.warn('le fichier '+cheminFichier[cheminFichier.length-1] + ' a été supprimé');
+                    }
+                });
+            }
+        }
+        var doc = files.ARTICLEDOC.name!=='' ? nomFichier : fields.ARTICLEDOCNAME;
+         //console.log(doc);
         Article.update(fields.ARTICLEID, fields.USERID, fields.CATEGORYID, fields.ARTICLENAME, fields.ARTICLEDATE, fields.ARTICLEDESC, doc, function () {
             req.flash('success', "Article modifié avec succès !")
             res.redirect('/articles')
@@ -79,13 +118,34 @@ router.post('/doEdit', ensureAuthenticated, function (req, res) {
 
     })
     form.on('fileBegin', function (name, file) {
-        if(file.name!=='')
-            file.path = path.join(uploadDir, file.name)
-
+        if(file.name!=='') {
+            var name = 'fichier';
+            const [fileName, fileExt] = file.name.split('.')
+            file.path = path.join(uploadDir, `${name}_${new Date().getTime()}.${fileExt}`);
+        }else{
+            var name='fileTodelete';
+            file.path = path.join(uploadDir, name);
+            console.log( file.path)
+        }
 
     })
 });
 
+router.get('/delete/:articleid/:docname',ensureAuthenticated, function (req, res) {
+    Article.delete(req.params.articleid, function (elem) {
+        if(req.params.docname.toString()!=='sansFichier'){
+            fs.unlink('public/uploads/'+req.params.docname, function (err) {
+                if (err) {
+                    console.error(err.toString());
+                } else {
+                    console.warn('le fichier '+req.params.docname + ' a été supprimé');
+                }
+            });
+        }
+
+        res.redirect('/articles');
+    })
+});
 // Access Control
 function ensureAuthenticated(req, res, next){
     if(req.isAuthenticated()){
